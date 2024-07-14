fa2bafb604e085e3d194f654f8bc331752799e7c
EirikSorhus <eirik.sorhus@gmail.com>
2024-07-14 10:53:07 +0200

    Change of folder name from python village to python_village

--- python village/Variables_and_some_arithmetic.py
+++
@@ -1,7 +0,0 @@
-def triangle_hyp(a, b):
-  hyp_sqrd = a**2 + b**2
-  return hyp_sqrd
-# whats better blue or green gutter? Iwould rather have neither
-print(triangle_hyp(3,4))
-
-## Test of branching.
