244ff516064b32db6178bcfb3ba3fcd28a2cb5d9
EirikSorhus <eirik.sorhus@gmail.com>
2024-07-14 10:31:53 +0200

    Adding strings and lists

---
+++ python village/strings_and_lists.py
@@ -0,0 +1,4 @@
+def sep_str(strng, a, b, c,d): # function which returns parts of the string, position a-b and c-d with a space in between
+  str_1 = strng[a:b]
+  str_2 = strng[c:d]
+  return str_1 + " " + str_2
