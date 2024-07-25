9e83fd3a712c02ee67fefa03be6843a2ac2dd55d
EirikSorhus <eirik.sorhus@gmail.com>
2024-07-25 14:01:51 +0200

    Changed counting_dna_nucleotides to using dictionary and added argparse to use in powershell

--- bioinformatics_stronghold/counting_dna_nucleotides.py
+++ bioinformatics_stronghold/counting_dna_nucleotides.py
@@ -1,27 +1,33 @@
 # Code for counting number of A, T, C and G in a string
-# Could use a dictionary instead of adding to local numbers
 import warnings
 
 def count_nucleotides(string):
-  A = 0
-  T = 0
-  C = 0
-  G = 0
+    counts = {'A': 0, 'T': 0, 'C': 0, 'G': 0}
+    
+    warning_issued = False
+    
+    # Count occurrences of each nucleotide
+    for e in string:
+        if e in counts:
+            counts[e] += 1
+        else:
+            if not warning_issued:
+                warnings.warn("Unexpected character(s) in string, should only contain A, T, C and G")
+                warning_issued = True
+    
+    A_count = counts['A']
+    C_count = counts['C']
+    G_count = counts['G']
+    T_count = counts['T']
+    
+    # Return the counts as a formatted string
+    return f"{A_count} {C_count} {G_count} {T_count}"
 
-  warning_issued = False
+if __name__ == '__main__':
+    import argparse
 
-  for e in string:
-    if e == "A":
-      A += 1
-    elif e == "T":
-      T += 1
-    elif e == "C":
-      C += 1
-    elif e == "G":
-      G += 1
-    else: 
-      if not warning_issued:
-        warnings.warn("Unexpected charecter/s in string, should only contain A, T, C or G")
-        warning_issued = True
+    parser = argparse.ArgumentParser(description= "Function to count the number of times A, C, G, T occur, and returns the occurances in order of A, C, G and T")
+    parser.add_argument("string", type = str, help = "string containing the nucleotides A, C, G, T")
+    args = parser.parse_args()
 
-  return print(A, T, C, G) # remove print if function is to be used in an other function.
+    print(count_nucleotides(args.string))
